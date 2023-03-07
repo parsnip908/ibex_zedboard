b8e99b560b0e399ebde38ed5de005483c722f7b1
Homealone7 <115061027+Homealone7@users.noreply.github.com>
2023-03-06 16:42:30 -0800

    Add files via upload

--- FPU/class.v
+++ FPU/class.v
@@ -1,13 +1,16 @@
 module class(
-	input [15:0] num,
+	input [15:0] Num,
 	output Inf,
 	output Neg_Inf,
-	output Nan,
+	output NaN,
+	output Sub_Norm,
 	output Normal
 	);
 
-	assign Inf = (num == 16'b0111_1111_1000_0000);
-	assign Neg_Inf = (num == 16'b1111_1111_1000_0000);
-	assign Nan = (num[14:7] == 8'b1111_1111) && (num[6:0]);
-	assign Normal = ~(Inf || Neg_Inf || Nan);
+	assign Inf = (Num == 16'b0111_1111_1000_0000);
+	assign Neg_Inf = (Num == 16'b1111_1111_1000_0000);
+	assign NaN = (Num[14:7] == 8'b1111_1111 && Num[6:0]);
+	assign Sub_Norm = (Num[14:7] == 8'b0000_0000);
+	assign Normal = ~(Inf || Neg_Inf || NaN || Sub_Norm);
+	
 endmodule
--- FPU/test_16.v
+++ FPU/test_16.v
@@ -14,70 +14,73 @@ reg [9:0] sig_a, sig_b, sig_c;
 reg [6:0] sig_cc;
 
 reg comparison;
-reg [15:0] Array1[25:0];
-reg [15:0] Array2[25:0];
-reg [15:0] Output_ref[25:0];
-reg [15:0] Output[25:0];
+reg [15:0] Array1[26:0];
+reg [15:0] Array2[26:0];
+reg [15:0] Output_ref[26:0];
+reg [15:0] Output[26:0];
 reg [4:0] counter;
 //reg [4:0] counter_c;
 wire Inf;
 wire Neg_Inf;
 wire NaN;
 wire Normal;
+wire Sub_Norm;
 
 wire  Inf_B;
 wire Neg_Inf_B;
 wire NaN_B;
 wire Normal_B;
+wire Sub_Norm_B;
 
 reg clk;
 integer i;
 
-  initial 
-    begin
-    inst = 1; 
-	 $readmemb("Array1.txt",Array1);
-    $readmemb("Array2.txt",Array2);
-    $readmemb("Sum.txt",Output_ref);
-	 A <= Array1[0];
-	 B <= Array2[0];
-    counter = 0;
-//	 counter_c = 0;
-	 comparison = 0;
-	 clk = 0;
-	 
-	 end
+initial begin
+	inst = 1; 
+	$readmemb("Array1.txt",Array1);
+	$readmemb("Array2.txt",Array2);
+	$readmemb("Sum.txt",Output_ref);
+	A <= Array1[0];
+	B <= Array2[0];
+	counter = 0;
+	comparison = 0;
+	clk = 0; 
+end
 
-	always begin
+always begin
 	#10
 	clk = ~clk;
-	end
+end
 
-	class class_A(.num(A), .Inf(Inf), .Neg_Inf(Neg_Inf), .Nan(NaN), .Normal(Normal));
-	class class_B(.num(B), .Inf(Inf_B), .Neg_Inf(Neg_Inf_B), .Nan(NaN_B), .Normal(Normal_B));
-	always @(posedge clk) begin
-		A <= Array1[counter+1];
-		B <= Array2[counter+1];
-		counter <= counter + 1;
-		Output[counter] <= C;
-		$display("C = %b, C_ref = %b", C, Output_ref[counter]);
-		if (counter == 26) begin
-			comparison = 1'b0;
-			for(i=0;i<26;i=i+1) begin
-				if (Output_ref[i] != Output[i]) begin
-				  $display("Mismatch at number %d", i);
-				  comparison = 1'b1;
-				end  
-			end
-			if (comparison == 1'b0) begin
-				$display("\nsuccess :)");
-			end
-			$stop;
+	class class_A(.Num(A), .Inf(Inf), .Neg_Inf(Neg_Inf), .NaN(NaN), .Normal(Normal), .Sub_Norm(Sub_Norm));
+	class class_B(.Num(B), .Inf(Inf_B), .Neg_Inf(Neg_Inf_B), .NaN(NaN_B), .Normal(Normal_B), .Sub_Norm(Sub_Norm_B));
+	
+always @(posedge clk) begin
+	A <= Array1[counter+1];
+	B <= Array2[counter+1];
+	counter <= counter + 1;
+	Output[counter] <= C;
+	$display("C = %b, C_ref = %b", C, Output_ref[counter]);
+	
+	if (counter == 26) begin
+		comparison = 1'b0;
+		for(i=0;i<27;i=i+1) begin
+			if (Output_ref[i] != Output[i]) begin
+				$display("Mismatch at number %d", i);
+				comparison = 1'b1;
+			end  
 		end
+		
+		if (comparison == 1'b0) begin
+			$display("\nsuccess :)");
+		end
+		$stop;
 	end
-  
-  
-  always @(*) begin
+	
+end
+
+
+always @(*) begin
 	
 	if (NaN || NaN_B) 
 		C = 16'b 0111111111000000;
@@ -91,22 +94,19 @@ integer i;
 				C = 16'b 1111_1111_1000_0000;
 		end
 	end
+	
 	else begin
-		
-	  //if (Normal == 1) begin
-	  //////////// Add or Sub //////////	
+		//////////// Add or Sub //////////	
 		add_sub = (inst == 1)? 1:0;
-	  
-	  
 	  //////////// Assign bigger number to a //////////
-	  {a, b, sign} = (A[14:0] > B[14:0])? {A, B, 1'b 0}: {B, A,  1'b 1};
-	  sign_a = a[15];
+		{a, b, sign} = (A[14:0] > B[14:0])? {A, B, 1'b 0}: {B, A,  1'b 1};
+		sign_a = a[15];
 		exp_a = a[14:7];
-		sig_a = {2'b 01,a[6:0], 1'b 0};
-
+		sig_a = (Sub_Norm)? {2'b 00,a[6:0], 1'b 0}:{2'b 01,a[6:0], 1'b 0};
+		
 		sign_b = b[15];
 		exp_b = b[14:7];
-		sig_b = {2'b 01,b[6:0], 1'b 0};
+		sig_b = (Sub_Norm_B)? {2'b 00,b[6:0], 1'b 0}:{2'b 01,b[6:0], 1'b 0};
 		shift = (a[14:7] == b[14:7])? 0: (a[14:7] - b[14:7]);
 		sig_b = sig_b >> shift;
 		
@@ -124,6 +124,7 @@ integer i;
 			C = {sign_c, exp_c, sig_cc};
 		  end
 		 end 
+		 
 		 //////////// Sub //////////
 		if (add_sub == 0) begin 
 			sign_c = (sign == 0)? sign_a: 1'b 1;
@@ -156,13 +157,10 @@ integer i;
 				sig_cc = (sig_c[0] == 1)? ((sig_c[1] == 1)? sig_c[7:1] + 1:sig_c[7:1]) : sig_c[7:1];
 				exp_c = exp_a - shift_sub;
 				C = {sign_c, exp_c, sig_cc};
-	//			 counter_c = counter + 1;
 			end
 		end
 	end
-  $display("sig_a = %b, sig_b = %b, C = %b, shift = %d, shift_sub = %d", sig_a, sig_b, C, shift, shift_sub);
-	end
-	//else 
-		//$display("Enter a valid number");  
-  
+
+end
+ 
 endmodule 
