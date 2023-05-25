396e9a998d975856f707ca9f0a6277cac776c7f3
parsnip908 <pkharche@ucdavis.edu>
2023-05-24 18:41:52 -0700

    sim test for other instructions

--- sw/sim/sim.c
+++ sw/sim/sim.c
@@ -43,36 +43,48 @@ int main(int argc, char **argv) {
   // "stack" are connected to the LEDs of the board.
   volatile uint8_t *var = (volatile uint8_t *) 0x0000c010;
   // volatile uint8_t *result = (volatile uint8_t *) 0x0000c020;
-  *var = 0x00;
-  // usleep(1000 * 1000); // 1000 ms
-  *var = 0xFF;
-  // usleep(1000 * 1000); // 1000 ms
-  *var = 0x00;
-  // usleep(1000 * 1000); // 1000 ms
 
-  // uint16_t num = 0xAA;
+  *var = 0xFF;
+  *var = 0x00;
+
   asm volatile(
-    "nop\n"
+    "li t1, 0x40860000\n"
+    "li t2, 0x40DD0000\n"
+    "fmv.w.x ft1, t1\n"
+    "fmv.w.x ft2, t2\n"
+    "fadd.s ft3, ft2, ft1\n"
+    "fmul.s ft4, ft2, ft1\n"
+    "fsub.s ft2, ft0, ft2\n"
+    "fsgnj.s ft5, ft1, ft2\n"
+    "fsgnjn.s ft5, ft1, ft2\n"
+    "fsgnjx.s ft5, ft2, ft2\n"
+    "fmin.s ft6, ft1, ft2\n"
+    "fmax.s ft6, ft1, ft2\n"
+    "fmv.w.x ft1, x0\n"    
+    "fmv.w.x ft2, x0\n"    
+    "fmv.w.x ft3, x0\n"    
+    "fmv.w.x ft4, x0\n"    
+    "fmv.w.x ft5, x0\n"    
+    "fmv.w.x ft6, x0\n"    
   );
 
-  // uint16_t data[4] = {0x4167,0xc169,0x3f10,0x4008};
+  // FCVT.W.S
+  // FCVT.WU.S
+  // FMV.X.W
+  // FEQ.S
+  // FLT.S
+  // FLE.S
+  // FCLASS.S
+  // FCVT.S.W
+  // FCVT.S.WU
+  // FMV.W.X
 
-  // volatile uint16_t *A = (volatile uint16_t *) 0x0000c020;
-  // volatile uint16_t *B = (volatile uint16_t *) 0x0000c030;
-  // volatile uint16_t *C = (volatile uint16_t *) 0x0000c040;
 
-  // for(int i = 0; i<4; i++)
-  // {
-  //   A[i] = data[i];
-  //   B[i] = data[i];
-  //   C[i] = 0;
-  // }
 
   volatile uint16_t **Matricies = (volatile uint16_t **) 0x0000c020;
   Matricies[0] = matrixA;
   Matricies[1] = matrixB;
   Matricies[2] = matrixC;
-
 
   asm volatile(
     "  li t0, 16\n"
@@ -83,6 +95,9 @@ int main(int argc, char **argv) {
     "  lw t6, 4(t5)\n"
     "  lw a7, 8(t5)\n"
     "  lw t5, 0(t5)\n"
+    "  addi t6, t6, -2\n"
+    "  addi a7, a7, -2\n"
+    "  addi t5, t5, -2\n"
     // "  li t6, 0xc030\n"
     // "  li a7, 0xc040\n"
     "fori:\n"
@@ -92,19 +107,19 @@ int main(int argc, char **argv) {
     "  slli t4, t1, 3\n"
     "  add t4, t4, t2\n"
     "  add t4, a7, t4\n"
-    "  flh ft3, 0(t4)\n"
+    "  flw ft3, 0(t4)\n"
     // "  fsub.h ft0, ft0, ft0\n"
     "fork:\n"
     "  slli t4, t1, 3\n"
     "  add t4, t4, t3\n"
     "  add t4, t5, t4\n"
-    "  flh ft1, 0(t4)\n"
+    "  flw ft1, 0(t4)\n"
     "  slli t4, t3, 3\n"
     "  add t4, t4, t2\n"
     "  add t4, t6, t4\n"
-    "  flh ft2, 0(t4)\n"
-    "  fmul.h ft1, ft1, ft2\n"
-    "  fadd.h ft0, ft0, ft1\n"
+    "  flw ft2, 0(t4)\n"
+    "  fmul.s ft1, ft1, ft2\n"
+    "  fadd.s ft0, ft0, ft1\n"
 
     "  addi t3, t3, 2\n"
     "  bne t3, t0, fork\n"
@@ -113,48 +128,32 @@ int main(int argc, char **argv) {
     "  add t4, t4, t2\n"
     "  add t4, a7, t4\n"
     "  fsh ft0, 0(t4)\n"
-    "  fsub.h ft0, ft0, ft3\n"
-    "  fadd.h ft4, ft0, ft4\n"
+    "  fsub.s ft0, ft0, ft3\n"
+    "  fadd.s ft4, ft0, ft4\n"
 
     "  addi t2, t2, 2\n"
     "  bne t2, t0, forj\n"
 
     "  addi t1, t1, 2\n"
     "  bne t1, t0, fori\n"
-      // "li t1, 13604\n"
-      // "li t2, 24193\n"
-      // "li t3, 49184\n"
-      // "sh t1, 0(t3)\n"
-      // "sh t2, 2(t3)\n"
-      // "flh ft1, 0(t3)\n"
-      // "flh ft2, 2(t3)\n"
-      // "fadd.h ft3, ft2, ft1\n"
-      // "fsh ft3, 4(t3)\n"
   );
 
   *var = 0xFF;
-  // usleep(1000 * 1000); // 1000 ms
   *var = 0x00;
-  // usleep(1000 * 1000); // 1000 ms
 
 
   uint16_t* num_p = (uint16_t *)(0xC024);
   uint16_t num = *num_p;
 
-  //0xA00A;//
 
+
+    *var = (uint8_t)(num & 0xFF);
+
+    *var = (uint8_t)(num >> 8);
+
+    // *var = 0x00;
+    // *var = 0xFF;
   while (1) {
-
-    // usleep(1000 * 1000); // 1000 ms
-    *var = num & 0xFF;
-
-    // usleep(1000 * 1000); // 1000 ms
-    *var = num >> 8;
-
-    // usleep(1000 * 1000); // 1000 ms
-    *var = 0x00;
-    // usleep(1000 * 1000); // 1000 ms
-    *var = 0xFF;
   }
     usleep(1000 * 1000); // 1000 ms
 }
